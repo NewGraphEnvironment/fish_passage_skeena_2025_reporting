d04cbbe3e02c52ba8ee60c7945df440882a8d1a3
newgraph-lschick <lucy@newgraphenvironment.com>
2024-12-11 17:10:44 -0800

    updated script for peace 2024 as per https://github.com/NewGraphEnvironment/fish_passage_template_reporting/issues/46  and to use for other 2024 repos.
    Former-commit-id: e195588fc661fcefe896a3026fce1cf8d191b2d1

--- scripts/01_prep_inputs/0130_pscis_export_to_template.R
+++ scripts/01_prep_inputs/0130_pscis_export_to_template.R
@@ -1,17 +1,15 @@
 # Export pscis data to csv for cut and paste into PSCIS submission spreadsheet
 
-# this is a weird workflow b/c we have two different projects and want to combine them for one submission.
+# Currently updated for Peace 2024
 
-path1 <- "~/Projects/gis/sern_lchl_necr_fran_2023/data_field/2023/form_pscis_2023.gpkg"
-dir_backup1 = "data/backup/sern_lchl_necr_fran_2023/"
-path2 <- "~/Projects/gis/sern_simpcw_2023/data_field/2023/form_pscis_2023.gpkg"
-dir_backup2 = "data/backup/sern_simpcw_2023/"
+path <- "~/Projects/gis/sern_peace_fwcp_2023/data_field/2024/form_pscis_2024.gpkg"
+dir_backup = "data/backup/"
 
 # read in cleaned form from Q after review and finalization
 # first we back up the gpkg in the repo and update the coordinate columns in the gpkg in QGIS
-pscis_export_raw1 <- fpr::fpr_sp_gpkg_backup(
-  dir_backup = dir_backup1,
-  path_gpkg = path1,
+pscis_export_raw <- fpr::fpr_sp_gpkg_backup(
+  dir_backup = dir_backup,
+  path_gpkg = path,
   update_utm = TRUE,
   update_site_id = TRUE,
   write_back_to_path = FALSE,
@@ -22,52 +20,46 @@ pscis_export_raw1 <- fpr::fpr_sp_gpkg_backup(
   return_object = TRUE)
 
 
-pscis_export_raw2 <- fpr::fpr_sp_gpkg_backup(
-  dir_backup = dir_backup2,
-  path_gpkg = path2,
-  update_utm = TRUE,
-  update_site_id = TRUE,
-  write_back_to_path = FALSE,
-  write_to_csv = TRUE,
-  # this versions on git everytime due to metadata and can't be tracked visually. Should only be committed when
-  # csv is versioned
-  write_to_rdata = FALSE,
-  return_object = TRUE)
-
-setdiff(names(pscis_export_raw1), names(pscis_export_raw2))
-setdiff(names(pscis_export_raw2), names(pscis_export_raw1))
-
-pscis_export_raw <- dplyr::bind_rows(
-  pscis_export_raw1,
-  pscis_export_raw2
-)
-
 ## Check for duplicates
-dups <- pscis_export_raw %>%
-  group_by(site_id) %>%
+dups <- pscis_export_raw |>
+  group_by(site_id) |>
   filter(n() > 1)
 
-# Site 15600468 was duplicated, issue here https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/110
-
-## Adding in spell-checked comments
-clean_comments <-  read.csv('data/inputs_extracted/clean_comments.csv')
-
-pscis_export_raw_clean <-  dplyr::left_join(pscis_export_raw |>
-                                              select(-assessment_comment),
-                                            clean_comments,
-                                            join_by(site_id == my_crossing_reference)) |>
-  relocate(assessment_comment, .after = my_priority)
+# No duplicate sites for peace 2024
 
 
-# prep for csvs for cut and paste by subsetting columns to those in spreadsheet
-# this project is simplified greatly in that it has only phase 1 sites
-pscis_export <- pscis_export_raw_clean %>%
-  # Fix time zone, issue here https://github.com/NewGraphEnvironment/fish_passage_template_reporting/issues/18
-  dplyr::mutate(date_time_start_raw = date_time_start,
-         date_time_start = lubridate::force_tz(date_time_start_raw, tzone = "America/Vancouver"),
-         date_time_start = lubridate::with_tz(date_time_start, tzone = "UTC"),
-         # Get time to append to comments
-         date_time_start = lubridate::ymd_hms(date_time_start),
+# Not used in Peace 2024
+# ## Adding in spell-checked comments
+# clean_comments <-  read.csv('data/inputs_extracted/clean_comments.csv')
+#
+# pscis_export_raw_clean <-  dplyr::left_join(pscis_export_raw |>
+#                                               select(-assessment_comment),
+#                                             clean_comments,
+#                                             join_by(site_id == my_crossing_reference)) |>
+#   relocate(assessment_comment, .after = my_priority)
+
+
+### IF RUN, must update code below with pscis_export_raw_clean
+
+
+
+# Not used in Peace 2024
+# ## Fix time zone, issue here https://github.com/NewGraphEnvironment/fish_passage_template_reporting/issues/18
+# pscis_export_raw_clean2 <- pscis_export_raw |>
+# dplyr::mutate(date_time_start_raw = date_time_start,
+#               date_time_start = lubridate::force_tz(date_time_start_raw, tzone = "America/Vancouver"),
+#               date_time_start = lubridate::with_tz(date_time_start, tzone = "UTC"))
+
+
+### IF RUN, must update code below with pscis_export_raw_clean2
+
+
+
+
+## prep for cut and paste to csvs by subsetting columns to those in spreadsheet
+pscis_export <- pscis_export_raw %>%
+  # Get time to append to comments
+  dplyr::mutate(date_time_start = lubridate::ymd_hms(date_time_start),
          date = lubridate::date(date_time_start),
          time = hms::as_hms(date_time_start)) |>
   # append moti ids to comments, differentiate between highway major structure, and add time to end
@@ -81,124 +73,51 @@ pscis_export <- pscis_export_raw_clean %>%
   dplyr::mutate(assessment_comment = paste0(assessment_comment, '. ', time)) |>
   # ditch time column
   dplyr::select(-time) |>
-  # only select columns from template object site_id and date_time_start
+  # add in pscis phase
+  dplyr::mutate(pscis_phase = case_when(
+    assess_type_phase1 == "Yes" ~ "phase 1",
+    assess_type_phase2 == "Yes" ~ "phase 2",
+    assess_type_reassessment == "Yes" ~ "reassessment")) |>
+  # only select columns from the template object, as well as site_id, pscis_phase, and date_time_start
   dplyr::select(
     dplyr::any_of(names(fpr::fpr_xref_template_pscis())),
     site_id,
-    date_time_start
+    date_time_start,
+    pscis_phase
   ) %>%
   # remove scoring columns, as these can't be copied and pasted anyways because of macros
   dplyr::select(-stream_width_ratio:-barrier_result) %>%
   sf::st_drop_geometry() %>%
-  # arrange so easy to copy/paste
-  dplyr::arrange(crossing_type,
+  # arrange by phase so easy to copy/paste into correct spreadsheet
+  dplyr::arrange(pscis_phase,
+                 crossing_type,
                  continuous_embeddedment_yes_no,
                  backwatered_yes_no,
                  crew_members,
                  date_time_start)
 
-# write to the imports_extracted dir. This is data we import to the project but they are extracted from other places.
+# write to the imports_extracted directory. This is data we import to the project but they are extracted from other places.
 dir.create("data/inputs_extracted")
 pscis_export %>%
   readr::write_csv('data/inputs_extracted/pscis_export_submission.csv', na='')
 
+# Now copy/paste all phase 1, phase 2, and reassessment data from `pscis_export_submission.csv` into separate pscis spreadsheets
+# (For example: pscis_phase1.xlsm, pscis_phase2.xlsm, pscis_reassessments.xlsm) which can be downloaded here
+# https://www2.gov.bc.ca/gov/content/environment/plants-animals-ecosystems/fish/aquatic-habitat-management/fish-passage/fish-passage-technical/assessment-projects.
+
+
+# After the data has been copy/pasted from `pscis_export_submission.csv` into the appropriate pscis spreadsheet, we can
+# calculate the structure, type, and size `sfpr_structure_size_type`.
+# Finally, copy/paste the data from `str_type_pscis1.csv`, `str_type_pscis2.csv`, and `str_type_pscis_reassessments.csv`
+# back into the appropriate pscis spreadsheet
 
 ## Add Structure, type, and size
-
-# Only phase 1 assessments in this project
 # fpr_import_pscis_all backs up to flatfile (csv)
 pscis_list <- fpr_import_pscis_all()
 pscis_phase1 <- pscis_list %>% pluck('pscis_phase1')
+pscis_phase2 <- pscis_list %>% pluck('pscis_phase2')
+pscis_reassessments <- pscis_list %>% pluck('pscis_reassessments')
 
 # Get structure, type, and size
-lfpr_structure_size_type(pscis_phase1)
-
-################################################################################################################
-#--------------------------------------------------fix surveyor initials---------------------------------------------------
-################################################################################################################
-# simpcw had no surveyor initials so needs case when
-
-path1 <- "~/Projects/gis/sern_lchl_necr_fran_2023/data_field/2023/form_pscis_2023.gpkg"
-dir_backup1 = "data/backup/sern_lchl_necr_fran_2023/"
-path2 <- "~/Projects/gis/sern_simpcw_2023/data_field/2023/form_pscis_2023.gpkg"
-dir_backup2 = "data/backup/sern_simpcw_2023/"
-
-# read in cleaned form from Q after review and finalization
-# first we back up the gpkg in the repo and update the coordinate columns in the gpkg in QGIS
-pscis_export_raw1 <- fpr::fpr_sp_gpkg_backup(
-  dir_backup = dir_backup1,
-  path_gpkg = path1,
-  update_utm = TRUE,
-  update_site_id = TRUE,
-  write_back_to_path = FALSE,
-  write_to_csv = FALSE,
-  # this versions on git everytime due to metadata and can't be tracked visually. Should only be committed when
-  # csv is versioned
-  write_to_rdata = FALSE,
-  return_object = TRUE)
-
-
-pscis_export_raw2 <- fpr::fpr_sp_gpkg_backup(
-  dir_backup = dir_backup2,
-  path_gpkg = path2,
-  update_utm = TRUE,
-  update_site_id = TRUE,
-  write_back_to_path = FALSE,
-  write_to_csv = FALSE,
-  # this versions on git everytime due to metadata and can't be tracked visually. Should only be committed when
-  # csv is versioned
-  write_to_rdata = FALSE,
-  return_object = TRUE)
-
-
-p_raw  <- dplyr::bind_rows(
-  pscis_export_raw1,
-  pscis_export_raw2
-) |>
-  sf::st_drop_geometry()
-
-# case when the surveyor initials
-p_fixed <- p_raw |>
-  dplyr::mutate(crew_members = dplyr::case_when(mergin_user == "newgraph_airvine" ~ "AI",
-                                                TRUE ~ "MW")
-  )
-
-# check this covered them all
-unique(p_fixed$crew_members)
-
-# read in the csv that was used to make the form and get rid of the duplicate as mentio
-c <- readr::read_csv(
-  'data/inputs_extracted/pscis_export_submission.csv'
-) |>
-  # remove the duplicate crossing 15600468
-  dplyr::distinct(my_crossing_reference, .keep_all = TRUE) |>
-  dplyr::select(-geom)
-
-# join the new data, case_when into place, remove "duplicate" renamed column and fix the "medium"
-c_fixed <- dplyr::left_join(
-  c,
-  p_fixed |> dplyr::select(my_crossing_reference, crew_members2 = crew_members),
-  by = "my_crossing_reference"
-) |>
-  # watch out to keep what is there already by only updateing when it is na yo.
-  dplyr::mutate(crew_members = dplyr::case_when(
-    is.na(crew_members) & !is.na(crew_members2) ~ crew_members2,
-                TRUE ~ crew_members)) |>
-  dplyr::select(-crew_members2)
-
-#burn local to test after ignoring it
-usethis::use_git_ignore('test.csv')
-
-# learned a bunch through this test.  was good to do
-c_fixed |>
-  readr::write_csv(
-    'test.csv', na = ''
-  )
-
-
-# ok - looks good finally - burn over the old one
-c_fixed |>
-  readr::write_csv(
-    'data/inputs_extracted/pscis_export_submission.csv', na = ''
-  )
+sfpr_structure_size_type(pscis_phase1)
 
